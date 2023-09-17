4e0bc8b484ea04fed92b26e7433cde9f41364590
tripm <triminhphan11800@gmail.com>
2023-09-17 19:25:08 +0700

    add model discount

---
+++ src/models/discount.model.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const mongoose = require('mongoose'); // Erase if already required
+
+const DOCUMENT_NAME = 'Discount'
+const COLLECTION_NAME = 'discounts'
+
+// Declare the Schema of the Mongo model
+const discountSchema = new mongoose.Schema({
+    discount_name: { type: String, required: true },
+    discount_description: {
+        type: String,
+        required: true
+    },
+    discount_type: { type: String, default: 'fixed_amount' },
+    discount_value: { type: Number, required: true },
+    discount_code: { type: String, required: true },
+    discount_start_date: { type: Date, required: true },
+    discount_end_date: { type: Date, required: true },
+    discount_max_uses: { type: Number, required: true },
+    discount_uses_count: { type: Number, required: true },
+    discount_users_used: { type: Array, default: [] },
+    discount_max_uses_per_user: { type: Number, required: true },
+    discount_min_oder_value: { type: Number, required: true },
+    discount_shopId: { type: mongoose.Schema.Types.ObjectId, ref: "Shop" },
+
+    discount_is_active: { type: Boolean, default: true },
+    discount_applies: { type: String, required: true, enum: ['all', 'specific'] },
+    discount_product_ids: { type: Array, default: [] },
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+});
+
+//Export the model
+module.exports = {
+    discount: mongoose.model(DOCUMENT_NAME, discountSchema)
+}
